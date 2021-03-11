143a04d7242bc6746c8f73ebf74522eb6fa76917
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-03-11 21:10:20 +0100

    get all pokemon

--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const data = require('../data/data')
+
+
+// Delete one 
+router.delete('/', (req, res, next) => {
+  const englishName = req.body.name
+  const newListofPokemon = data.module.filter((one) => one.name.english !== englishName)
+  res.status(201).json(newListofPokemon)
+}) 
+
+// Create one
+router.post('/', (req, res, next) => {
+  const { name, type, base } = req.body;
+  const newPokemon = {
+    id: data.module.length + 1,
+    name,
+    type,
+    base
+  }
+  data.module.push(newPokemon)
+  res.status(201).json(newPokemon)
 });
+
+
+// GET all pokemon 
+router.get('/', function(req, res, next) {
+  res.status(201).json(data.module)
+});
+
 
 module.exports = router;
